d43d9595a8ab1bb51c44307f1f3e600e003c0b4c
skoop-n8n-continue <admin1+n8n-continue@skoop.digital>
2026-08-17 19:10:37 +0000

    update

--- fix-css.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let css = fs.readFileSync('styles.css', 'utf8');
-css = css.replace('.item-price {', '.item-price {\n    min-width: 30%;\n    white-space: nowrap;');
-fs.writeFileSync('styles.css', css);
